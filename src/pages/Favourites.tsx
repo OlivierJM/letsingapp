c9b0dc04624a31658b3209bc4fe7e494b7cf37ea
OlivierJM <manolivier93@gmail.com>
2020-03-28 21:51:47 +0200

    add contents to pages

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import '../theme/Favourites.css'
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonImg,
+  IonIcon,
+  IonCol
+} from "@ionic/react";
+import "../theme/Favourites.css";
 
 const Favourites: React.FC = () => {
   return (
@@ -16,9 +29,24 @@ const Favourites: React.FC = () => {
             <IonTitle size="large">Favourites</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonCol className="ion-justify-content-between">
+          <IonContent>
+            <IonList>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
+                <IonItem key={i}>
+                  <IonAvatar slot="start">
+                    <IonImg alt='to be play button' src="https://images.unsplash.com/photo-1484972759836-b93f9ef2b293?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" />
+                  </IonAvatar>
+                  <IonLabel>
+                    <h2>{i} Song on the playlist</h2>
+                  </IonLabel>
+                </IonItem>
+              ))}
+            </IonList>
+          </IonContent>
+        </IonCol>
       </IonContent>
     </IonPage>
   );
 };
-
 export default Favourites;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
             {[1, 2, 3, 4].map(i => (
               <IonCol size="6" key={i}>
                 <IonCard>
-                    <IonImg src={'https://images.unsplash.com/photo-1510172951991-856a654063f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'} />
+                    <IonImg alt='album cover' src={'https://images.unsplash.com/photo-1510172951991-856a654063f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'} />
                   <IonCardContent>
                     This is content, without any paragraph or header tags,
                     within an ion-cardContent element.
