1121385b4358b46e7e4f07776295f210d74a6a2b
OlivierJM <manolivier93@gmail.com>
2020-06-17 22:09:21 +0200

    add more enhancement

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient, { gql } from "apollo-boost";
+import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  uri: process.env.REACT_APP_SERVER_URL,
 });
 
 ReactDOM.render(
