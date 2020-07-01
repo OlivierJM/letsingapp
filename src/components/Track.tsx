33f69e171478bd171d3d5bc34b934d98bfc66986
OlivierJM <manolivier93@gmail.com>
2020-07-01 09:44:38 +0200

    add a few share options

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -6,12 +6,18 @@ import {
   IonButton,
   IonIcon,
   IonList,
+  IonActionSheet,
 } from "@ionic/react";
 import "../theme/Track.css";
 import {
   ellipsisVerticalOutline,
   createOutline,
   readerOutline,
+  shareSocialOutline,
+  close,
+  logoFacebook,
+  logoWhatsapp,
+  logoTwitter,
 } from "ionicons/icons";
 import { useHistory } from "react-router";
 
@@ -38,7 +44,8 @@ const Track: React.FC<TrackType> = ({
     event: undefined,
   });
   const history = useHistory();
-  // onClick={viewLyrics}
+  const [showActionSheet, setShowActionSheet] = useState<boolean>(false);
+  const currentUrl = window.location.href
   return (
     <>
       <IonItem>
@@ -82,6 +89,20 @@ const Track: React.FC<TrackType> = ({
                   Edit Song
                 </IonLabel>
               </IonItem>
+              <IonItem
+                onClick={() => {
+                  setShowActionSheet(true);
+                  setShowPopover({ open: false, event: undefined });
+                }}
+              >
+                <IonLabel>
+                  <IonIcon
+                    style={{ marginBottom: -2 }}
+                    icon={shareSocialOutline}
+                  />{" "}
+                  Share Song
+                </IonLabel>
+              </IonItem>
             </IonList>
           </IonPopover>
 
@@ -95,6 +116,47 @@ const Track: React.FC<TrackType> = ({
             <IonIcon icon={ellipsisVerticalOutline} />
           </IonButton>
         </IonLabel>
+        <IonActionSheet
+          isOpen={showActionSheet}
+          onDidDismiss={() => setShowActionSheet(false)}
+          cssClass="my-custom-class"
+          buttons={[
+            {
+              text: "Facebook",
+              icon: logoFacebook,
+              handler: () => {
+                window.open(
+                  `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}/${songId}`
+                );
+              },
+            },
+            {
+              text: "Whatsapp",
+              icon: logoWhatsapp,
+              handler: () => {
+               window.open(`https://wa.me/?text=${currentUrl}/${songId}`);
+              },
+
+            },
+            {
+              text: "Twitter",
+              icon: logoTwitter,
+              handler: () => {
+                window.open(
+                  `https://twitter.com/intent/tweet?text=${currentUrl}/${songId}`
+                );
+              },
+            },
+            {
+              text: "Cancel",
+              icon: close,
+              role: "cancel",
+              handler: () => {
+                console.log("Cancel clicked");
+              },
+            },
+          ]}
+        ></IonActionSheet>
       </IonItem>
     </>
   );
